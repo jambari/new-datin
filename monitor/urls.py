9448b458a8fa3c68b172ddc986f2a886cb6cd14f
Jambari <jambari@bmkg.go.id>
2026-04-23 14:12:35 +0900

    feat: remove Integrasi (seismicity_analysis) from sidebar and URL

--- monitor/urls.py
+++ monitor/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('api/receive/', views.receive_event_data, name='receive_event'),
     path('monitor/json/', views.export_all_json, name='export_json'),
     path('monitor/webgis/<str:event_id>/', views.webgis_event_detail, name='webgis_detail'),
-    path('monitor/analisis/', views.seismicity_analysis, name='seismicity_analysis'),
 
 ]
